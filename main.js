d4d0077ea99db90a0c5796fce5844c1ec9e2f39f
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-02-13 11:53:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,104 +1,27 @@
-const socket = io();
+const connection = new WebSocket('ws://localhost:8080');
 
-const inboxPeople = document.querySelector(".inbox__people");
-const inputField = document.querySelector(".message_form__input");
-const messageForm = document.querySelector(".message_form");
-const messageBox = document.querySelector(".messages__history");
-const fallback = document.querySelector(".fallback");
-
-let userName = "";
-
-const newUserConnected = (user) => {
-  userName = user || `User${Math.floor(Math.random() * 1000000)}`;
-  socket.emit("new user", userName);
-  addToUsersBox(userName);
+connection.onopen = () => {
+  console.log('connected');
 };
 
-const addToUsersBox = (userName) => {
-  if (!!document.querySelector(`.${userName}-userlist`)) {
-    return;
-  }
-
-  const userBox = `
-    <div class="chat_ib ${userName}-userlist">
-      <h5>${userName}</h5>
-    </div>
-  `;
-  inboxPeople.innerHTML += userBox;
+connection.onclose = () => {
+  console.error('disconnected');
 };
 
-const addNewMessage = ({ user, message }) => {
-  const time = new Date();
-  const formattedTime = time.toLocaleString("en-US", { hour: "numeric", minute: "numeric" });
-
-  const receivedMsg = `
-  <div class="incoming__message">
-    <div class="received__message">
-      <p>${message}</p>
-      <div class="message__info">
-        <span class="message__author">${user}</span>
-        <span class="time_date">${formattedTime}</span>
-      </div>
-    </div>
-  </div>`;
-
-  const myMsg = `
-  <div class="outgoing__message">
-    <div class="sent__message">
-      <p>${message}</p>
-      <div class="message__info">
-        <span class="time_date">${formattedTime}</span>
-      </div>
-    </div>
-  </div>`;
-
-  messageBox.innerHTML += user === userName ? myMsg : receivedMsg;
+connection.onerror = error => {
+  console.error('failed to connect', error);
 };
 
-// new user is created so we generate nickname and emit event
-newUserConnected();
+connection.onmessage = event => {
+  console.log('received', event.data);
+  let li = document.createElement('li');
+  li.innerText = event.data;
+  document.querySelector('#chat').append(li);
+};
 
-messageForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (!inputField.value) {
-    return;
-  }
-
-  socket.emit("chat message", {
-    message: inputField.value,
-    nick: userName,
-  });
-
-  inputField.value = "";
-});
-
-inputField.addEventListener("keyup", () => {
-  socket.emit("typing", {
-    isTyping: inputField.value.length > 0,
-    nick: userName,
-  });
-});
-
-socket.on("new user", function (data) {
-  data.map((user) => addToUsersBox(user));
-});
-
-socket.on("user disconnected", function (userName) {
-  document.querySelector(`.${userName}-userlist`).remove();
-});
-
-socket.on("chat message", function (data) {
-  addNewMessage({ user: data.nick, message: data.message });
-});
-
-
-socket.on("typing", function (data) {
-  const { isTyping, nick } = data;
-
-  if (!isTyping) {
-    fallback.innerHTML = "";
-    return;
-  }
-
-  fallback.innerHTML = `<p>${nick} is typing...</p>`;
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+  let message = document.querySelector('#message').value;
+  connection.send(message);
+  document.querySelector('#message').value = '';
 });
