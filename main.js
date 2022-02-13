f4a074346b7ae3e81cdd28642561e3174b86c804
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-02-13 20:02:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const connection = new WebSocket('ws://localhost:8080');
-var http = require('http'); // Import Node.js core module
+ar http = require('http'); // Import Node.js core module
 
 var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/asd') { //check the URL of the current request
+    if (req.url == '/') { //check the URL of the current request
         
         // set response header
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
@@ -12,11 +12,28 @@ var server = http.createServer(function (req, res) {   //create web server
         res.end();
     
     }
+    else if (req.url == "/student") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.end();
     
+    }
+    else if (req.url == "/admin") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.end();
+    
+    }
+    else
+        res.end('Invalid Request!');
+
+});
+
 server.listen(5000); //6 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')    
-
+console.log('Node.js web server at port 5000 is running..')
 
 connection.onopen = () => {
   console.log('connected');
