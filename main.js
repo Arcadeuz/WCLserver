768d608a20cfe00764add0b4b31b73613bfdc5d3
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-02-14 13:12:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile('/index.html');
 });
 
 const PORT = process.env.PORT || 3001;
