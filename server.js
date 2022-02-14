6f100f6bc15c542a14b599957a0475ba6485397e
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-02-14 20:53:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
-const WebSocket = require('ws')
+const WebSocket = require('ws');
+
+const webSocketServer = new WebSocket.Server({ port: 8080 });
+
+webSocketServer.on('connection', webSocket => {
+  webSocket.on('message', message => {
+    console.log('Received:', message);
+    broadcast(message);
+  });
+});
+
+function broadcast(data) {
+  webSocketServer.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+}
+
+
+
+
+
+
+/*const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 8080 },()=>{
     console.log('server started')
 })
@@ -10,4 +34,4 @@ wss.on('connection', function connection(ws) {
 })
 wss.on('listening',()=>{
    console.log('listening on 8080')
-})
+})*/
