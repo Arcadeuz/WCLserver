52cef62f5b739cba21d8f5840dfe739a1bcf29c9
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-02-13 21:04:53 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const app = express();
- 
+
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
+  res.send("Hello World!");
 });
- 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
