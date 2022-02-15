1aec9085ab46c59fbd65add13ec20fa5110114fc
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-02-15 12:37:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,17 @@ const wsServer = new WebSocketServer({
 
 // Especificamos el puerto en una varibale port, incorporamos cors, express 
 // y la ruta a los archivo estáticos (la carpeta public)
-app.set("port", 3000);
+app.set("port", 3001);
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "")));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);*/
+
+//app.use(express.static(path.join(__dirname, "")));
 
 function originIsAllowed(origin) {
     // Para evitar cualquier conexión no permitida, validamos que 
