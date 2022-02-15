ff7b8a1c313fcd4733f459893c12355c5f4a63ac
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-02-15 10:52:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,41 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+
 //const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-//ws part
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
-
-
-const connection = new WebSocket('ws://localhost:8080');
-
-connection.onopen = () => {
-  console.log('connected');
-};
-
-connection.onclose = () => {
-  console.error('disconnected');
-};
-
-connection.onerror = error => {
-  console.error('failed to connect', error);
-};
-
-connection.onmessage = event => {
-  console.log('received', event.data);
-  let li = document.createElement('li');
-  li.innerText = event.data;
-  document.querySelector('#chat').append(li);
-};
-
-document.querySelector('form').addEventListener('submit', event => {
-  event.preventDefault();
-  let message = document.querySelector('#message').value;
-  connection.send(message);
-  document.querySelector('#message').value = '';
-});
