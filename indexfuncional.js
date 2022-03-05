a9c90b51d97b4e50c04abae1770f082272245ff6
Arcadeuz <64760914+Arcadeuz@users.noreply.github.com>
2022-03-05 19:04:16 -0300

    Update indexfuncional.js

--- indexfuncional.js
+++ indexfuncional.js
@@ -5,6 +5,16 @@ const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
+
 
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
